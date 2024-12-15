36ec46bbba38e99f17cd81734744677586d96c0c
Rifky Alfarez <207006052@student.unsil.ac.id>
2024-12-15 11:13:08 +0700

    feat(app): add hero section

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,15 +3,15 @@ import Badge from './Badge';
 
 export default function Navbar() {
   return (
-    <div className="w-full bg-neutral-50 border-b border-neutral-200">
+    <div className="w-full bg-neutral-50 border-b border-pink-200">
       <div className="wrapper py-4 flex items-center justify-between">
         <div className="flex items-center gap-x-2">
           <img
             src="/logo.png"
             alt="DuxStore Logo"
-            className="size-6 object-cover"
+            className="size-6 lg:size-7 object-cover"
           />
-          <h1 className="text-lg font-bold text-transparent bg-gradient-to-r from-pink-600 via-pink-500 to-pink-400 bg-clip-text">
+          <h1 className="text-lg lg:text-xl font-bold text-transparent bg-gradient-to-r from-pink-600 via-pink-500 to-pink-400 bg-clip-text">
             DuxStore
           </h1>
         </div>
